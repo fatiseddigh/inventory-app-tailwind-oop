176d99127d9a1bce9d4e37e7b03a80ca4d2c039c
fatiseddigh <seddighfati@gmail.com>
2023-06-13 14:13:53 +0430

    add toggle product

--- src/js/CategoryUi.js
+++ src/js/CategoryUi.js
@@ -2,10 +2,19 @@ import Storage from "./Storage.js";
 const titleCategory = document.querySelector("#category-title");
 const descriptionCategory = document.querySelector("#category-description");
 const addCategoryBtn = document.querySelector("#add-new-category");
+const toggleAddCategoryBtn = document.querySelector("#toggle-add-category");
+const categoryWrapper = document.querySelector("#category-wrapper");
+const cancelAddCategory = document.querySelector("#cancel-add-category");
 
 class CategoryUi {
   constructor() {
     addCategoryBtn.addEventListener("click", (e) => this.addNewCategory(e));
+    toggleAddCategoryBtn.addEventListener("click", (e) =>
+      this.toggleAddCategory(e)
+    );
+    cancelAddCategory.addEventListener("click", (e) =>
+      this.toggleAddCategory(e)
+    );
     this.categories = [];
   }
   setApp() {
@@ -22,6 +31,8 @@ class CategoryUi {
     this.createCategoryList();
     titleCategory.value = "";
     descriptionCategory.value = "";
+    categoryWrapper.classList.add("hidden");
+    toggleAddCategoryBtn.classList.hidden("hidden");
   }
   createCategoryList() {
     let result = `<option class="bg-violet-200 text-violet-950" value="">
@@ -35,6 +46,10 @@ class CategoryUi {
     const categoryDom = document.getElementById("product-category");
 
     categoryDom.innerHTML = result;
+  }
+  toggleAddCategory(e) {
+    categoryWrapper.classList.remove("hidden");
+    toggleAddCategoryBtn.classList.add("hidden");
   }
 }
 export default new CategoryUi();
